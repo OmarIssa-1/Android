fa58f0b5f2bf3fa8d0fee1d2caa5c460b6ad4596
Nuhamin Enbakum <nuham1941@gmail.com>
2025-05-15 06:23:11 +0300

    Update clock_settime_implementation.c

--- clock_settime_implementation.c
+++ clock_settime_implementation.c
@@ -1,1 +1,17 @@
-#includ<stdio.h>
+#include<stdio.h>
+#include<time.h>
+#include<errno.h>
+
+int main(){
+  struct timespace new_time;
+new_time.tv_sec = 174704400;
+new_time.tv_nsec = 0;
+
+if(clock_settime(CLOCK_REALTIME, &new_time) == -1){
+perror("Clock_settime failed!");
+return 1;
+}
+printf("System time successfully updated!\n");
+return 0;
+}
+  
